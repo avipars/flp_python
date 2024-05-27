15b1e68e32c6316bded662629d2f4076e747bf0d
Avi Parshan <avi.pars@gmail.com>
2024-05-27 19:13:09 +0300

    s

--- excercise2/question7_twin.py
+++ excercise2/question7_twin.py
@@ -1,22 +1,34 @@
 from helpers import *
 from eratosthenes import napa
-#twin primes
+# twin primes
 # 1 number matters, it and the twin
 # 1 to n
-# allprimes btwn 1 and n, make a dict has 1 value 
+# allprimes btwn 1 and n, make a dict has 1 value
 # keys: all prime # btwn 1 and 100
-# values = list of twin primes 
+# values = list of twin primes
 # 3,5,
 
-def is_twin(n):
-    fn = lambda x: x-2 or x+2
-    
+
+def remove_non_twins(n):
+    all_prims = napa(n)  # list of all primes
+    def fn(x): return x+2 in all_prims
+    return list(filter(fn, all_prims))
+
+
+def convert_to_dict(lst):
+    def fn(x): return x+2
+    return dict(zip(lst, map(fn, lst)))
+
 def twinp(n):
-    #dict 
-    # return list(filter(form, range(1, n+1)))
-    all_prims = napa(n) #list of all primes
-    
-    list(filter())
+    lst = remove_non_twins(n)
+    dct = convert_to_dict(lst)
+    return format_twin(dct)
+
+def printer(key, val):
+    print(f"key: {key}, val")
+def format_twin(dct):
+    # functional way to print stuff in dic
+
 def main():
     num = getAndProcessInput()
     if num is None:
@@ -24,6 +36,7 @@ def main():
     else:
         print(f"Actual solution requested:")
         print(twinp(num))
-        
+
+
 if __name__ == "__main__":
     main()
