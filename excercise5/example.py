ce3a359a8017c44da81593531ae785899bd08405
Avi Parshan <avi.pars@gmail.com>
2024-06-10 15:36:22 +0300

    d

---
+++ excercise5/example.py
@@ -0,0 +1,12 @@
+def rev_str(my_str):
+    length = len(my_str)
+    for i in range(length - 1, -1, -1):
+        yield my_str[i]
+
+
+for char in rev_str("hello"):
+    print(char, end="")
+    
+print("\n")
+st = "hello"
+print(st[::-1])
